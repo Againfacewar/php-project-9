3754ab0e088c4b09168b71f6026578b7f41b379c
Ucsus <ucsus@exbo.ru>
2024-10-05 16:29:46 +0300

    add main page

--- public/index.php
+++ public/index.php
@@ -10,19 +10,29 @@ use Slim\Http\Response;
 use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Psr7\Request;
 use Slim\Views\PhpRenderer;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 $container = new Container();
-$container->set('renderer', function () {
-    return new PhpRenderer(__DIR__ . '/../resources/views');
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../resources/views');
 });
 $container->set('flash', function () {
     return new Messages();
 });
 $app = AppFactory::createFromContainer($container);
 $app->add(MethodOverrideMiddleware::class);
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+// Add other middleware
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Hello, hexlet!');
-});
+    $twig = $this->get(Twig::class);
+
+    return $twig->render($response, 'main.html.twig');
+})->setName('home');
 
 $app->run();
